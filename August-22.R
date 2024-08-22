a8f11e554306caf23a58f7336dac6864ce74e484
IanAAdams <142043517+IanAAdams@users.noreply.github.com>
2024-08-22 16:24:02 -0400

    Add files via upload

---
+++ August-22.R
@@ -0,0 +1,12 @@
+# Author: Ian Adams, Date: 08/22/24, Purpose: Test the measures of descriptive stats
+
+# How to calculate standard deviation in R
+
+X <- c(1,2,3,4,5)
+sd(X)
+
+# How to calculate mean in R
+mean(X)
+
+# How to calculate the median in R 
+median(X)
